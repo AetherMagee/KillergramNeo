421dfad642e49b05e218eed0f024d09ab874d7a3
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-03-31 12:27:34 +0300

    chore: Bump version to 0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "aether.killergram.neo"
         minSdk = 27
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 3
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
