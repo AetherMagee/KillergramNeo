fe65c9e761f9e2f0b6956480dc47ce56d06e21a7
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-08-25 15:49:06 +0300

    fix: Disable resource shrinking for now
    It's nuking the Solar icons from the app as they're not used

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
-            isShrinkResources = true
+            isShrinkResources = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
