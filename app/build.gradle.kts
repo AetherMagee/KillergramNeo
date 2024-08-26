05ffd2bc8e6a9fd3c171eb46cfaa76a74585a224
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-08-26 16:24:07 +0300

    chore: Bump version to 0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "aether.killergram.neo"
         minSdk = 27
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.5"
+        versionCode = 7
+        versionName = "0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
